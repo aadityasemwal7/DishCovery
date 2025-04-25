82c8b62ea766e27cb55da8a19c6f09f836485fc5
aadityasemwal7 <aadityasemwalbca2020@gmail.com>
2025-04-25 14:01:44 +0530

    first commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,24 @@
+import Logo from "../assets/logo.png"
+
+const Header = () => {
+  return (
+    <div>
+        <nav>
+            <ul className="flex items-center cursor-pointer font-semibold">
+                <li className="mr-auto"><div><img width={130} src={Logo}></img></div></li>
+                <li className="mx-2">Popular Recipes</li>
+                <li className="mx-5 ml-auto">search</li>
+                <li className="mx-3"><button className="px-6 py-2 hover:bg-white rounded-lg hover:text-red-600 bg-red-600 text-white transition ease-in-out duration-300">
+                Login
+                </button></li>
+                <li className="mx-3"><button className="px-6 py-2 hover:bg-white rounded-lg hover:text-red-600 bg-red-600 text-white transition ease-in-out duration-300">
+                SignUp
+                </button></li>
+            </ul>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Header
